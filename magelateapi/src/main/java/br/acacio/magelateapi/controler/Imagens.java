1e41521162a314af6a4358204dd16c4d091fbd93
Suzane Quintino <acacio.junio28@gmail.com>
2024-07-20 14:35:17 -0300

    Montando o save no controller.

--- magelateapi/src/main/java/br/acacio/magelateapi/controler/Imagens.java
+++ magelateapi/src/main/java/br/acacio/magelateapi/controler/Imagens.java
@@ -3,6 +3,7 @@ package br.acacio.magelateapi.controler;
 import java.io.IOException;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,12 +11,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import br.acacio.magelateapi.model.ImagemModel;
+import br.acacio.magelateapi.service.ServiceImagem;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/v1/imagens")
 @Slf4j
 public class Imagens{
+@Autowired
+public ServiceImagem service;
 
 @PostMapping
 public ResponseEntity save(
@@ -25,8 +30,14 @@ public ResponseEntity save(
             ) throws IOException {
 
         log.info("Imagem recebida: name: {}, size: {}", file.getOriginalFilename(), file.getSize());
-        log.info("Nome definido para a imagem:{}",name);
-        log.info("tags:{}",tags);
+        ImagemModel imagem= ImagemModel.builder()
+        .name(name)
+        .file()
+        .size(file.getSize())
+        .tags(String.join(",", tags))
+        .extension(file.getContentType())
+        .build(); 
+        service.save(imagem);
 
         return ResponseEntity.ok().build();
     }
